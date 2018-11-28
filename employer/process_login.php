15cc2151841aea223efdb3037f2e73904c0ecdc8
Simran Senapati <32230315+simran-senapati@users.noreply.github.com>
2018-11-28 17:46:47 +0530

    Add files via upload

---
+++ employer/process_login.php
@@ -0,0 +1,30 @@
+<?php
+include_once('config.php');
+
+$email=$_POST['email'];
+$passd=$_POST['password'];
+$query=mysqli_query($db1,"select * from login where email='$email'");
+$result=mysqli_fetch_array($query,MYSQLI_ASSOC);
+
+if(($result>0) && ( password_verify( $passd, $result['password'] ) ) ){
+    if($result['usertype']=="jobseeker")
+    {
+        session_start();
+        $_SESSION["id"] = $result['log_id'];
+        $_SESSION["type"] = $result['usertype'];
+        header('location:jobseeker/profile.php?msg=success');
+    }
+ elseif($result['usertype']=="employer")
+ {
+     session_start();
+     $_SESSION["elogid"] = $result['log_id'];
+     $_SESSION["type"] = $result['usertype'];
+     $_SESSION["status"]=$result['status'];
+     header('location:employer/profile.php?msg=success');
+ }
+}
+else
+{
+header('location:login.php?msg=failed');
+}
+?>
